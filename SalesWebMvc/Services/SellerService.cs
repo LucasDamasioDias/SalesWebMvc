d6f6875f974b0f818a9c056679f678f30b8e9898
Lucas Damásio <lucas_damasio@hotmail.com>
2025-01-15 22:32:50 -0300

    SellerService and basic FindAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _contex;
+
+        public SellerService(SalesWebMvcContext contex)
+        {
+            _contex = contex;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _contex.Seller.ToList();
+        }
+    }
+}
